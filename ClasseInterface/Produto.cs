decfa58e3ced48818424cf10d601c721ba70ff2f
Lucas Leonardo de Jesus de Melo <39694603870@jpmorgan.sp>
2017-11-14 20:57:46 -0200

    first commit

---
+++ ClasseInterface/Produto.cs
@@ -0,0 +1,38 @@
+namespace ClasseInterface
+{
+    public class Produto : IAcao
+    {
+       public int Id { get; set; } 
+       public string Nome { get; set; }
+       public string Descricao { get; set; }
+       public double Preco { get; set; }
+
+       public Produto()
+       {
+           
+       }
+       public Produto(int Id, string Nome, string Descricao, double Preco)
+       {
+           this.Id = Id;
+           this.Nome = Nome;
+           this.Descricao = Descricao;
+           this.Preco = Preco;
+
+       }
+
+        public string Cadastro()
+        {
+            string composicao = "Id do produto: " + Id +
+                                "\nNome do produto: " + Nome.ToUpper()+
+                                "\nDescrição do produto: " + Descricao + 
+                                "\nPreço do produto: " + string.Format("{c}", Preco);
+                                
+            return "Produto cadastrado com sucesso/n" + composicao;
+        }
+
+        public string Consulta()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
